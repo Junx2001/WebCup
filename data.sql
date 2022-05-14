f53a51583888a3ea42ec99741b0fe1107b246c91
unknown <rafa.rotsy@gmail.com>
2022-05-14 17:50:46 +0300

    nanova sidebar

--- data.sql
+++ data.sql
@@ -1,4 +1,8 @@
 -- creation de tables
+CREATE TABLE typeForfait(
+     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     nom varchar(100)
+);
 
 CREATE TABLE lieu(
      id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -9,7 +13,7 @@ CREATE TABLE lieu(
 
 CREATE TABLE forfait(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type varchar(100),
+    idType int,
     prix float,
     tailleMin float,
     typePayement varchar(100),
@@ -24,10 +28,25 @@ CREATE TABLE administrateur(
     mdp varchar(250)
 );
 
-ALTER TABLE forfait ADD FOREIGN KEY (idLieu) REFERENCES lieu(id);
+    ALTER TABLE forfait ADD CONSTRAINT `forfait_fk`   
+    FOREIGN KEY(idType) REFERENCES typeForfait (id) ON DELETE  CASCADE;  
+    ALTER TABLE forfait ADD CONSTRAINT `forfait1_fk`   
+    FOREIGN KEY(idType) REFERENCES typeForfait (id) ON UPDATE CASCADE;  
+
+    
+    ALTER TABLE forfait ADD CONSTRAINT `forfait2_fk`   
+    FOREIGN KEY(idLieu) REFERENCES lieu (id) ON DELETE  CASCADE;  
+    ALTER TABLE forfait ADD CONSTRAINT `forfait3_fk`   
+    FOREIGN KEY(idLieu) REFERENCES lieu (id) ON UPDATE CASCADE;  
+
 
 -- insertion de données
 INSERT INTO administrateur VALUES (1,'admin@gmail.com',sha1('motdepasse123'));
+
+INSERT INTO typeForfait VALUES(1,'surface');
+INSERT INTO typeForfait VALUES(2,'sous-terre');
+INSERT INTO typeForfait VALUES(3,'eau');
+INSERT INTO typeForfait VALUES(4,'espace');
 
 INSERT INTO lieu VALUES (1,'Madagascar',46.869107,-18.766947);
 INSERT INTO lieu VALUES (2,'Maurice',57.552152,-20.348404);
@@ -39,21 +58,21 @@ INSERT INTO lieu VALUES (7,'Océan Atlantique',-1.3225897148440247,-28.866028191
 INSERT INTO lieu VALUES (8,'Lune',null,null);
 INSERT INTO lieu VALUES (9,'Mars',null,null);
 
-INSERT INTO forfait VALUES (null,'surface',25,20,'abonnement mensuel',5,1,'normal');
-INSERT INTO forfait VALUES (null,'surface',25,20,'abonnement mensuel',5,3,'normal');
-INSERT INTO forfait VALUES (null,'surface',50,150,'abonnement mensuel',10,1,'normal');
-INSERT INTO forfait VALUES (null,'surface',50,150,'abonnement mensuel',10,2,'normal');
-INSERT INTO forfait VALUES (null,'sous-terre',65,150,'abonnement mensuel',10,4,'medium');
-INSERT INTO forfait VALUES (null,'sous-terre',35,20,'abonnement mensuel',5,1,'medium');
+INSERT INTO forfait VALUES (null,1,25,20,'abonnement mensuel',5,1,'normal');
+INSERT INTO forfait VALUES (null,1,25,20,'abonnement mensuel',5,3,'normal');
+INSERT INTO forfait VALUES (null,1,50,150,'abonnement mensuel',10,1,'normal');
+INSERT INTO forfait VALUES (null,1,50,150,'abonnement mensuel',10,2,'normal');
+INSERT INTO forfait VALUES (null,2,65,150,'abonnement mensuel',10,4,'medium');
+INSERT INTO forfait VALUES (null,2,35,20,'abonnement mensuel',5,1,'medium');
 
-INSERT INTO forfait VALUES (null,'eau',530,90,'payement à vie',12,6,'high');
-INSERT INTO forfait VALUES (null,'eau',600,100,'payement à vie',10,6,'high');
-INSERT INTO forfait VALUES (null,'eau',550,95,'payement à vie',10,7,'high');
-INSERT INTO forfait VALUES (null,'eau',200,50,'payement à vie',5,7,'high');
+INSERT INTO forfait VALUES (null,3,530,90,'payement à vie',12,6,'high');
+INSERT INTO forfait VALUES (null,3,600,100,'payement à vie',10,6,'high');
+INSERT INTO forfait VALUES (null,3,550,95,'payement à vie',10,7,'high');
+INSERT INTO forfait VALUES (null,3,200,50,'payement à vie',5,7,'high');
 
-INSERT INTO forfait VALUES (null,'espace',999,50,'payement à vie',20,8,'premium');
-INSERT INTO forfait VALUES (null,'espace',750,35,'payement à vie',20,8,'premium');
-INSERT INTO forfait VALUES (null,'espace',1500,80,'payement à vie',20,9,'premium');
-INSERT INTO forfait VALUES (null,'espace',999,30,'payement à vie',20,9,'premium');
+INSERT INTO forfait VALUES (null,4,999,50,'payement à vie',20,8,'premium');
+INSERT INTO forfait VALUES (null,4,750,35,'payement à vie',20,8,'premium');
+INSERT INTO forfait VALUES (null,4,1500,80,'payement à vie',20,9,'premium');
+INSERT INTO forfait VALUES (null,4,999,30,'payement à vie',20,9,'premium');
 
 
